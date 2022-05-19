99a23cc4f1113e4b04fcf4911efeff243569bfec
laurell <laurellmurray@gmail.com>
2022-05-19 18:04:10 -0400

    login required

--- sugahfree/views.py
+++ sugahfree/views.py
@@ -5,25 +5,23 @@ from django.shortcuts import render, redirect, get_object_or_404
 from myapi.models import Input
 from .forms import InputForm
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     user = request.user
-    # input = Input.objects.all()
     if request.method == "POST":
         form = InputForm(request.POST)
         if form.is_valid():
             input = form.save(commit=False)
             input.user = request.user
             input.created = timezone.now()
-            
             input.save() 
-            
             return redirect('main_list')
     else:
         form = InputForm()
     return render(request, 'sugahfree/home.html', {'form': form, 'user': user})
 
-
+@login_required
 def edit(request, pk):
     input = get_object_or_404(Input, pk=pk)
     if request.method == "POST":
@@ -37,43 +35,21 @@ def edit(request, pk):
         form = InputForm(instance = input)
     return render(request, 'sugahfree/edit.html', {'form': form, 'user': request.user})        
 
+@login_required
 def redirect_to_list():
     return redirect("main_list")
 
+@login_required
 def delete(request, pk):
     input = get_object_or_404(Input, pk=pk)
     input.delete()
   
     return redirect("main_list")
 
+@login_required
 def main_list(request):
     inputs = Input.objects.filter(user=request.user).order_by('created')
     return render(request, 'sugahfree/main_list.html', {'inputs': inputs})
 
 
-    
-
-# def ajax_add_input(request):
-#     data = {}
-#     if request.method == "POST":
-#         print(request.POST)
-#         insulin_in_units = request.POST.get('insulin_in_units')
-#         carbs_in_grams = request.POST.get('carbs_in_grams')
-#         food_intake = request.POST.get('food_intake')
-
-#         form = InputForm(request.POST)
-
-#         if form.is_vaild():
-#             new_input = form.save()
-#             data['saved'] = True
-#         else:
-#             data['saved'] = False
-#         data['insulin_in_units'] = insulin_in_units
-#         data['carbs_in_grams'] = carbs_in_grams
-#         data['food_intake'] = food_intake
-
-#     else:
-#         data['response': 'nothing to get']
-#     return JsonResponse(data)
-
-
+   
